a82d17319d49f50a36b5d9d132047db2c2add849
Anwesh43 <anweshthecool0@gmail.com>
2018-06-15 09:12:19 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsidearcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsidearcview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.kotlinlinkedsidearcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.linkedsidearcview.LinkedSideArcView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LinkedSideArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
