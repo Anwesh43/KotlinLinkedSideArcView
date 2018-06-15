3c0760294422ecaaf87d322226262e81c163cb1c
Anwesh43 <anweshthecool0@gmail.com>
2018-06-15 08:42:05 +0530

    adding LinkedSideArc class

--- linkedsidearcview/src/main/java/com/anwesh/uiprojects/linkedsidearcview/LinkedSideArcView.kt
+++ linkedsidearcview/src/main/java/com/anwesh/uiprojects/linkedsidearcview/LinkedSideArcView.kt
@@ -129,4 +129,28 @@ class LinkedSideArcView (ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LinkedSideArc(var i : Int) {
+
+        private var curr : SideArcNode = SideArcNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
