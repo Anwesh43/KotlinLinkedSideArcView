d5ba6191c0930113088707dbb3a791c60c97d161
Anwesh43 <anweshthecool0@gmail.com>
2018-06-15 08:57:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsidearcview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedsidearcview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedsidearcview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedsidearcview.LinkedSideArcView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedSideArcView.create(this)
     }
 }
